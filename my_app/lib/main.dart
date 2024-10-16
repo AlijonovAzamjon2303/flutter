9518e31903e74cd22e6252cc1ee26014b1e61f83
Azamjon <Alijonovrobo@gmail.com>
2024-10-16 09:47:01 +0500

    updated

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -5,6 +5,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,17 +23,21 @@ class MyHomePage extends StatelessWidget {
   final List<String> items =
       List<String>.generate(100, (index) => 'Item $index');
 
+  MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ListView Example'),
+        title: const Text('ListView Example'),
       ),
       body: ListView.builder(
         itemCount: items.length,
         itemBuilder: (context, index) {
+          bool toq = index % 2 == 0;
           return ListTile(
-            title: Text(items[index]),
+            title: Text(items[index],
+                style: TextStyle(color: toq ? Colors.blue : Colors.red)),
           );
         },
       ),
